bd1c4b82018d333d6d1b4c1d0c179f164d7ac1c9
bmgcosta <bmgcosta@outlook.com>
2022-03-30 19:43:57 +0100

    Change name controller Audi -> AudiController

--- app/Http/Controllers/AudiController.php
+++ app/Http/Controllers/AudiController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class Audi extends Controller
+class AudiController extends Controller
 {
     public function index()
     {
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-Route::get('/', 'Audi@index');
+Route::get('/', 'AudiController@index');
 
 Auth::routes();
 
