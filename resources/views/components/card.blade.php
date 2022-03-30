03959e2d0bf2db218c3ff2b6e04388b4afc25b27
bmgcosta <bmgcosta@outlook.com>
2022-03-30 17:48:11 +0100

    Add code a single card with data img, title, desc

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,8 @@
+<div class="card" style="width: 18rem;">
+    <img src="{{$img}}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">{{$title}}</h5>
+      <p class="card-text">{{$desc}}</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+</div>
