1f3264b5dd04d3cde625f6ced3d9c2e56182402e
bmgcosta <bmgcosta@outlook.com>
2022-04-13 22:35:52 +0100

    Fix Error Title

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Player;
-use App\Classes\Text;
 use Illuminate\Http\Request;
 
 class PlayerController extends Controller
@@ -18,7 +17,6 @@ class PlayerController extends Controller
         $players = Player::all();
         //dd($players);
         return view('pages.players.index', [
-            'title' => Text::arrayTransform(array_keys($players->first()->attributesToArray())),
             'players' => $players
         ]);
     }
